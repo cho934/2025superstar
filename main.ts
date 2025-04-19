628683f3a1cb73591ce0edd5b6c6f1223829b879
Christophe CHAUDELET <cho934@users.noreply.github.com>
2025-04-19 02:00:36 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,98 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 44) {
+        tirette = 1
+    }
+})
+function StopMotors () {
+    maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
+    servos.P2.run(0)
+    servos.P2.stop()
+}
+input.onButtonPressed(Button.A, function () {
+    enabledetection = 0
+    GOGOGO()
+    StopMotors()
+    butiner()
+    enabledetection = 0
+})
+function butiner () {
+    servos.P2.run(30)
+}
+function GOGOGO () {
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
+    basic.pause(3000)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 50)
+    basic.pause(500)
+    if (color == 2) {
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 0)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
+        basic.pause(1000)
+    } else {
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 0)
+        basic.pause(1000)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 25)
+        basic.pause(1500)
+    }
+}
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "BLUE") {
+        color = 2
+    }
+    if (receivedString == "YELLOW") {
+        color = 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    color = 1
+    butiner()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    StopMotors()
+    servos.P1.setAngle(90)
+})
+let color = 0
+let tirette = 0
+let enabledetection = 0
+maqueenPlusV2.I2CInit()
+let countdetection = 0
+let dist = 0
+let readpin = 0
+serial.redirectToUSB()
+enabledetection = 0
+radio.setGroup(169)
+radio.setFrequencyBand(64)
+radio.setTransmitPower(7)
+tirette = 0
+color = 0
+servos.P2.run(0)
+basic.forever(function () {
+    while (tirette == 0) {
+        if (color == 1) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Skull)
+        }
+        if (color == 2) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Diamond)
+        }
+        if (color == 0) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Scissors)
+        }
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    basic.showIcon(IconNames.Angry)
+    basic.pause(2000)
+    enabledetection = 0
+    GOGOGO()
+    StopMotors()
+    enabledetection = 0
+    butiner()
+    tirette = 0
+    color = 0
+})
+basic.forever(function () {
+	
+})
